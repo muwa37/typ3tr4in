1544dd43e9c219c3a031928a586fe6d301f88f2c
muwa37 <yond41m3@icloud.com>
2024-09-07 22:01:20 +0400

    Fixed new text generating bug

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -1,6 +1,11 @@
 type Props = { onClickFn: () => void; buttonText: string; isActive?: boolean };
 
 const CustomButton = ({ onClickFn, buttonText, isActive }: Props) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onClickFn();
+    e.currentTarget.blur();
+  };
+
   return (
     <button
       className={`m-3 p-3 border-2 rounded-md   hover:border-blue-300 hover:text-blue-300 flex items-center justify-center italic ${
@@ -8,7 +13,7 @@ const CustomButton = ({ onClickFn, buttonText, isActive }: Props) => {
           ? 'border-lime-700 text-lime-700'
           : 'border-red-300 text-red-300'
       }`}
-      onClick={onClickFn}
+      onClick={handleClick}
     >
       {buttonText}
     </button>
