aac8fff712b7aee1426ba0a0d73d0f90ce4bbcd0
muwa37 <yond41m3@icloud.com>
2024-09-08 20:30:41 +0400

    ghpages fixes

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ if (rootElem) {
 
   root.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,28 +3,25 @@ import About from '@/pages/About';
 import Error from '@/pages/Error';
 import Home from '@/pages/Home';
 import Stat from '@/pages/Stat';
-import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <div>
-              <Main />
-            </div>
-          }
-        >
-          <Route path='/' index element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/stats' element={<Stat />} />
-          <Route path='*' element={<Error />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <div>
+            <Main />
+          </div>
+        }
+      >
+        <Route path='/' index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/stats' element={<Stat />} />
+        <Route path='*' element={<Error />} />
+      </Route>
+    </Routes>
   );
 };
 
