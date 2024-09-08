3269630527799f2ddc9ed7ba833224b7a442817e
muwa37 <yond41m3@icloud.com>
2024-09-08 19:52:45 +0400

    Prepared for ghpages

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,27 +1,31 @@
 import Main from '@/layouts/Main';
-import About from '@/pages/About';
-import Error from '@/pages/Error';
-import Home from '@/pages/Home';
-import Stat from '@/pages/Stat';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+const Home = React.lazy(() => import('@/pages/Home'));
+const About = React.lazy(() => import('@/pages/About'));
+const Stat = React.lazy(() => import('@/pages/Stat'));
+const Error = React.lazy(() => import('@/pages/Error'));
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route
-        path='/'
-        element={
-          <div>
-            <Main />
-          </div>
-        }
-      >
-        <Route path='/' index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/stats' element={<Stat />} />
-        <Route path='*' element={<Error />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <Main />
+            </div>
+          }
+        >
+          <Route path='/' index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/stats' element={<Stat />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
