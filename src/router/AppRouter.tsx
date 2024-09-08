7816bb2b79b6ccdffa684e103f0fa792fc481d3e
muwa37 <yond41m3@icloud.com>
2024-09-08 20:07:33 +0400

    Fixed for deploy

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,10 @@
 import Main from '@/layouts/Main';
-import React, { Suspense } from 'react';
+import About from '@/pages/About';
+import Error from '@/pages/Error';
+import Home from '@/pages/Home';
+import Stat from '@/pages/Stat';
+import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
-const Home = React.lazy(() => import('@/pages/Home'));
-const About = React.lazy(() => import('@/pages/About'));
-const Stat = React.lazy(() => import('@/pages/Stat'));
-const Error = React.lazy(() => import('@/pages/Error'));
 
 const AppRouter = () => {
   return (
